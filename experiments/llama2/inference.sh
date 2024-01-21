2d1f9fb35c38e7a644d2a5fdfd19acf83bfbcbf9
Amirhossein Abaskohi <amirhossein.abaskohi@gmail.com>
2024-01-21 02:14:35 -0800

    Update inference.sh

--- experiments/llama2/inference.sh
+++ experiments/llama2/inference.sh
@@ -1,4 +1,4 @@
-python3 your_inference_script.py \
+python3 inference.py \
     --max_length 500 \
     --temperature 0 \
     --model_name_or_path "./brain_teaser_explain_llama2_checkpoints/checkpoint-final/" \
