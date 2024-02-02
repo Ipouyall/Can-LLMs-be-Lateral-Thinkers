2d01ee1b16dbaeccc1a46d3c8337dca10b3720c6
pooyasadeghi <pouyasadeghi2012@gmail.com>
2024-02-02 04:03:08 +0330

    Add RAG measure script

---
+++ experiments/rag/benchmarks/measure_hits.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import glob
+
+
+def find_files(dir: str, ends_with: str):
+    return glob.glob(f"{dir}/*{ends_with}", recursive=True)
+
+def hit_ratio(df: pd.DataFrame):
+    variations = ["SR", "CR"]
+    ret_cols = [col for col in df.columns if 'retrieve' in col]
+    hits = 0
+    for _, row in df.iterrows():
+        org = row['org id'].split("_")[0]
+        sample_variations = [org] + [f"{org}_{var}" for var in variations]
+        for col in ret_cols:
+            if row[col] in sample_variations:
+                hits += 1
+    hit_rate = 3 * len(df)
+    hit_rate = hits / hit_rate
+    return hits, hit_rate
+
+
+
+
+if __name__ == "__main__":
+    files = find_files(".", ".csv")
+    print(f"{'File':<32}| {'Hits':<6}| {'Hit Rate'}")
+    for file in files:
+        df = pd.read_csv(file)
+        hits, hit_rate = hit_ratio(df)
+        print(f"{file[-30:]:<32}| {hits:<6}| {hit_rate:.3f}")
