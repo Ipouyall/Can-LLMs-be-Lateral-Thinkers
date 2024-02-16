69238d379edbbbefb5295aaf1267ab0d035a077c
pooyasadeghi <pouyasadeghi2012@gmail.com>
2024-02-16 22:15:34 +0330

    Add SWAG for FT line

--- experiments/finetune/inference_other.py
+++ experiments/finetune/inference_other.py
@@ -84,11 +84,12 @@ so announce the option you think is the best one in the format: \
     itr = zip(df['question'], df['choices']['text'], df['answerKey'])
 
     output_data = []
+    score = 0
 
     for que, options, key in tqdm(itr, total=150, desc="Inference (CSQA)"):
         data = {"question": que}
         for i, opt in enumerate(options, start=1):
-            data[f"option_{i+1}"] = opt
+            data[f"option_{i}"] = opt
         result = llm_chain.run(data).strip()
         data["zephyr_raw"] = result
         pred_key, pred = extract_csqa_answer(result)
@@ -96,7 +97,88 @@ so announce the option you think is the best one in the format: \
         data["answer"] = key
         data["score"] = 1 if pred_key == key else 0
         output_data.append(data)
-    save_inference(output_data, args.output_path)
+        if pred_key == key:
+            score += 1
+            print(f"CSQA Score: {round(score/1.5, 3)}%")
+    save_inference(output_data, "csqa_inference.jsonl")
+
+
+def get_inference_swag(args):
+    def extract_swag_answer(result: str):
+        o1 = result.rfind("Option 1")
+        o2 = result.rfind("Option 2")
+        o3 = result.rfind("Option 3")
+        o4 = result.rfind("Option 4")
+
+        answer = np.argmax([o1, o2, o3, o4])
+        return answer
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
+
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=args.model_name_or_path,
+        tokenizer=tokenizer,
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=True,
+        device_map="auto",
+        max_length=args.max_length,
+        eos_token_id=tokenizer.eos_token_id
+    )
+
+    llm = HuggingFacePipeline(pipeline=pipeline, model_kwargs={'temperature': args.temperature})
+
+    template = """ \
+I would provide you a question and four options. \
+The question is designed in common sense reasoning evaluation im completion style. \
+Give a prompt and four options, \
+you should find the option that could complete the prompt in commonsensical and logical way and being related to prompt. \
+You may need to think of the problem from another perspective to find the best answer.
+
+Prompt: "{question} ..."
+
+Option 1: "{option_1}"
+Option 2: "{option_2}"
+Option 3: "{option_3}"
+Option 4: "{option_4}"
+
+o answer this question, you should exactly mention one option, \
+so announce the option you think is the best one in the format: \
+'Option 1' or 'Option 2' or 'Option 3' or 'Option 4':
+"""
+    template = template.strip()
+
+    prompt = PromptTemplate(
+        template=template,
+        input_variables=["question", "option_1", "option_2", "option_3", "option_4"],
+    )
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+    df = load_dataset("swag", split='validation')[:150]
+    itr = zip(
+        df['startphrase'],
+        df['ending0'], df['ending1'], df['ending2'], df['ending3'],
+        df['label'],
+    )
+
+    output_data = []
+    score = 0
+
+    for que, *options, key in tqdm(itr, total=150, desc="Inference (CoDAH)"):
+        data = {"question": que}
+        for i, opt in enumerate(options, start=1):
+            data[f"option_{i}"] = opt
+        result = llm_chain.run(data).strip()
+        data["zephyr_raw"] = result
+        pred = extract_swag_answer(result)
+        data["zephyr"] = pred
+        data["answer"] = key
+        data["score"] = 1 if pred == key else 0
+        output_data.append(data)
+        if pred == key:
+            score += 1
+            print(f"CoDAH Score: {round(score/1.5, 3)}%")
+    save_inference(output_data, "swag_inference.jsonl")
 
 
 def save_inference(data, address):
@@ -108,3 +190,6 @@ def save_inference(data, address):
 if __name__ == "__main__":
     args = parse_arguments()
     get_inference_csqa(args)
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+    get_inference_swag(args)
---
+++ experiments/finetune/inference_other.sh
@@ -0,0 +1,4 @@
+python3 inference.py \
+    --max_length 2000 \
+    --temperature 0 \
+    --model_name_or_path "./brain_teaser_explain_zephyr_checkpoints/checkpoint-final/" \
